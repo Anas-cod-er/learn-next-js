9cc8dc2a8f1b6f05e8296dccd27c8c13fd06b04f
Anas-cod-er <anasbinjaman100@gmail.com>
2026-09-20 02:31:57 +0800

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <div className="flex flex-1 w-full items-center justify-center">
+      <h1>hello world</h1>
     </div>
   );
 }
