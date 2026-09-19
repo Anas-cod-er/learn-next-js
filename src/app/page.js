04623e754b76d679768a49e045e3e61fde34351e
Anas-cod-er <anasbinjaman100@gmail.com>
2026-09-20 02:55:32 +0800

    updated everything

--- src/app/page.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="flex flex-1 w-full items-center justify-center">
-      <h1>hello world</h1>
-    </div>
-  );
-}
--- src/app/vlog/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const vlog = () => {
-  return (
-    <div>This is vlog page</div>
-  )
-}
-
-export default vlog
